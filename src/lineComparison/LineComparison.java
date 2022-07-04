72affeb9bc7e5bb1a519092db59062ba3675ba91
Sharanya Gopal <sharanyagopal11@gmail.com>
2022-07-04 18:34:31 +0530

    welcome message

---
+++ src/lineComparison/LineComparison.java
@@ -0,0 +1,13 @@
+package lineComparison;
+
+public class LineComparison {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to line comparison program");
+
+	}
+
+}
